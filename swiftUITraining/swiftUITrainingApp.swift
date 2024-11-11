90f29316b9cf66111c97a97e8f25aec30e4fbe90
fdcmarch <fdc.crismarch@gmail.com>
2024-11-11 09:36:44 +0800

    Initial Commit

---
+++ swiftUITraining/swiftUITrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUITrainingApp.swift
+//  swiftUITraining
+//
+//  Created by FDC-CrisMarch on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUITrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
